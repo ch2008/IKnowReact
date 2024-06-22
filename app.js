56e589557efa42f02abb184fcede0b893be837e5
Chandra Das <chandrabali.das@gmail.com>
2024-06-22 11:12:25 +0530

    updated git ignore

--- app.js
+++ app.js
@@ -2,7 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const Root=ReactDOM.createRoot(document.getElementById("root"));
-console.log(Root);
+
+const hello=()=>{
+    return(
+        
+        <div>
+        <h1> hello from react </h1>
+        </div>
+        
+    )
+}
+ 
 
 
-
+Root.render(hello)
